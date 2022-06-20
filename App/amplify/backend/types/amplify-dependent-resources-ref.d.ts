a0de9928a199584ca7f41c17a1f188832ade1a39
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-06-20 19:16:24 +0300

    configure Amplify

---
+++ App/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "app": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
