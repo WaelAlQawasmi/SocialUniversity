7c065606fcd5894c54a729ed5663e2ad933ba185
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-06-21 17:25:25 +0300

    Design Activities

--- App/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ App/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,22 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "app96dd2c01": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3d7e468c2": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
